6c0bbefc08f007e742dcef0e9c00b1ecd1d99bd2
Advitnayak4311 <advithnayak1113@gmail.com>
2026-04-02 09:04:43 +0530

    Initial commit of HelloMaven project

---
+++ src/main/java/LAB8/prgm8/App.java
@@ -0,0 +1,13 @@
+package LAB8.prgm8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
